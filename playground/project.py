f6c5f526dc1259764a92ecc81da2781e54cfb088
Sumer Kohli <sumer.kohli@berkeley.edu>
2020-10-19 15:32:03 -0700

    Generalize execution and stitching code

--- playground/project.py
+++ playground/project.py
@@ -4,6 +4,7 @@ import open3d as o3d
 import cv2
 import argparse
 
+
 def project(img, disp_map, intrinsics):
     # Set camera parameters for reference in later calculations
 
@@ -16,7 +17,7 @@ def project(img, disp_map, intrinsics):
 
     # Read disparity map
     disparity = np.load(disp_map)[0]
-    depth_im = 1 / disparity # unscaled disparity to depth conversion
+    depth_im = 1 / disparity  # unscaled disparity to depth conversion
     print(depth_im.max(), depth_im.min(), depth_im.shape, color_im.shape)
 
     pts = []
@@ -37,29 +38,53 @@ def project(img, disp_map, intrinsics):
     colors = np.array(colors)
 
     return pts, colors
-    
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # parser.add_argument('--image', type=str)
     # parser.add_argument("--disp", type=str)
 
     # args = parser.parse_args()
     # project(args.image, args.disp)
-    int_1 = [1024, 768, 701.195, 699.50371766, 485.301, 432.17]
-    int_2 = [1024, 768, 695.313, 695.313 * 0.997147, 543.21, 375.81]
-    int_3 = [1024, 768, 669.93, 669.93 * 0.997553, 509.605, 358.812]
-    int_4 = [1024, 768, 674.319, 674.319 * 0.995272, 461.743, 398.09]
+    intrinsics = [
+        (
+            [1024, 768, 701.195, 699.50371766, 485.301, 432.17],
+            "0_{i+1}.jpg",
+            "0_{i+1}_disp.npy",
+        ),
+        (
+            [1024, 768, 695.313, 695.313 * 0.997147, 543.21, 375.81],
+            "0_{i+1}.jpg",
+            "0_{i+1}_disp.npy",
+        ),
+        (
+            [1024, 768, 669.93, 669.93 * 0.997553, 509.605, 358.812],
+            "0_{i+1}.jpg",
+            "0_{i+1}_disp.npy",
+        ),
+        (
+            [1024, 768, 674.319, 674.319 * 0.995272, 461.743, 398.09],
+            "0_{i+1}.jpg",
+            "0_{i+1}_disp.npy",
+        ),
+    ]
 
-    pts_1, colors_1 = project('0_1.jpg', '0_1_disp.npy', int_1)
-    pts_2, colors_2 = project('0_2.jpg', '0_2_disp.npy', int_2)
-    pts_3, colors_3 = project('0_3.jpg', '0_3_disp.npy', int_3)
-    pts_4, colors_4 = project('0_4.jpg', '0_4_disp.npy', int_4)
-    
     pcd = o3d.geometry.PointCloud()
+    pairs = []
+    for i, intr_tuple in enumerate(intrinsics):
+        intr, img, depth_map = intr_tuple
+        pairs.append(
+            project(eval('f"' + img + '"'), eval('f"' + depth_map + '"'), intr)
+        )
+
     # print(np.concatenate((pts_1, pts_2)).shape)
-    pcd.points = o3d.utility.Vector3dVector(np.concatenate((pts_1, pts_2, pts_3, pts_4)))
-    pcd.colors = o3d.utility.Vector3dVector(np.concatenate((colors_1, colors_2, colors_3, colors_4)))
+    pcd.points = o3d.utility.Vector3dVector(
+        np.concatenate(tuple(pair[0] for pair in pairs))
+    )
+    pcd.colors = o3d.utility.Vector3dVector(
+        np.concatenate(tuple(pair[1] for pair in pairs))
+    )
 
     # transform to view properly
     pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
