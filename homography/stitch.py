b7a750a18dc49f5839b13e8b6afbbab8fd08c633
maxemerling <maxemerling@gmail.com>
2020-10-31 12:07:20 -0700

    post merge on max's computer

--- homography/stitch.py
+++ homography/stitch.py
@@ -3,10 +3,19 @@ import os
 from glob import glob
 import numpy as np
 
+
+
 frames = [cv2.imread(f'result{i}.png') for i in range(4)]
 
-frames[1][:580, :630, :] = frames[0][:580, :630, :]
-frames[1][450:, :611, :] = frames[2][450:, :611, :]
-frames[1][260:, :788, :] = frames[3][260:, :788, :]
+result = np.zeros(frames[0].shape)
 
-cv2.imwrite("stitched.png", frames[1])
+result[:, 512:] = frames[0][:, 512:]
+#result[350:, 250:550] = frames[1][350:, 250:550]
+result[:, :512] = frames[3][:, :512]
+#result[200:600, :512] = frames[2][200:600, :512]
+
+# frames[1][:580, :630, :] = frames[0][:580, :630, :]
+# frames[1][450:, :611, :] = frames[2][450:, :611, :]
+# frames[1][260:, :788, :] = frames[3][260:, :788, :]
+
+cv2.imwrite("stitched.png", result)
