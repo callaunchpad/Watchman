6fbbc226ff09828ad5529fac04fa551c92ecfc5a
devangjhabakh <devangjhabakh@berkeley.edu>
2020-11-18 18:27:45 -0800

    Updated to save model globally and reuse

--- segmentation/detect.py
+++ segmentation/detect.py
@@ -20,210 +20,208 @@ from mit_semseg.config import cfg
 import wget
 
 if not os.path.isdir("ckpt"):
-    print("Hello!!!")
-    os.mkdir("ckpt")
-    os.mkdir("ckpt/ade20k-resnet50dilated-ppm_deepsup")
-    base_url = "http://sceneparsing.csail.mit.edu/model/pytorch/ade20k-resnet50dilated-ppm_deepsup/"
-    encoder = base_url + "encoder_epoch_20.pth"
-    decoder = base_url + "decoder_epoch_20.pth"
-    wget.download(encoder, "ckpt/ade20k-resnet50dilated-ppm_deepsup/encoder_epoch_20.pth")
-    wget.download(decoder, "ckpt/ade20k-resnet50dilated-ppm_deepsup/decoder_epoch_20.pth")
+	print("Hello!!!")
+	os.mkdir("ckpt")
+	os.mkdir("ckpt/ade20k-resnet50dilated-ppm_deepsup")
+	base_url = "http://sceneparsing.csail.mit.edu/model/pytorch/ade20k-resnet50dilated-ppm_deepsup/"
+	encoder = base_url + "encoder_epoch_20.pth"
+	decoder = base_url + "decoder_epoch_20.pth"
+	wget.download(encoder, "ckpt/ade20k-resnet50dilated-ppm_deepsup/encoder_epoch_20.pth")
+	wget.download(decoder, "ckpt/ade20k-resnet50dilated-ppm_deepsup/decoder_epoch_20.pth")
 
 colors = loadmat('data/color150.mat')['colors']
 names = {}
 with open('data/object150_info.csv') as f:
-        reader = csv.reader(f)
-        next(reader)
-        for row in reader:
-                names[int(row[0])] = row[5].split(";")[0]
-
+		reader = csv.reader(f)
+		next(reader)
+		for row in reader:
+				names[int(row[0])] = row[5].split(";")[0]
 
 def visualize_result(data, pred, cfg):
-        (img, info) = data
+		(img, info) = data
 
-        # print predictions in descending order
-        pred = np.int32(pred)
-        # print("The predictions are: ", pred, pred.shape)
-        pixs = pred.size
-        uniques, counts = np.unique(pred, return_counts=True)
-        # print("Predictions in [{}]:".format(info))
-        for idx in np.argsort(counts)[::-1]:
-                name = names[uniques[idx] + 1]
-                ratio = counts[idx] / pixs * 100
-                # if ratio > 0.1:
-                        # print("  {}: {:.2f}%".format(name, ratio))
+		# print predictions in descending order
+		pred = np.int32(pred)
+		# print("The predictions are: ", pred, pred.shape)
+		pixs = pred.size
+		uniques, counts = np.unique(pred, return_counts=True)
+		# print("Predictions in [{}]:".format(info))
+		for idx in np.argsort(counts)[::-1]:
+				name = names[uniques[idx] + 1]
+				ratio = counts[idx] / pixs * 100
+				# if ratio > 0.1:
+						# print("  {}: {:.2f}%".format(name, ratio))
 
-        # colorize prediction
-        pred_color = colorEncode(pred, colors).astype(np.uint8)
-        return pred_color.tolist()
-        print("Colorized: ", pred_color, pred_color.shape)
+		# colorize prediction
+		pred_color = colorEncode(pred, colors).astype(np.uint8)
+		return pred_color.tolist()
+		print("Colorized: ", pred_color, pred_color.shape)
 
-        # aggregate images and save
-        im_vis = np.concatenate((img, pred_color), axis=1)
+		# aggregate images and save
+		im_vis = np.concatenate((img, pred_color), axis=1)
 
-        img_name = info.split('/')[-1]
-        Image.fromarray(im_vis).save(
-                os.path.join(cfg.TEST.result, img_name.replace('.jpg', '.png')))
+		img_name = info.split('/')[-1]
+		Image.fromarray(im_vis).save(
+				os.path.join(cfg.TEST.result, img_name.replace('.jpg', '.png')))
 
 
 def test(segmentation_module, loader, gpu):
-        segmentation_module.eval()
+		segmentation_module.eval()
 
-        pbar = tqdm(total=len(loader))
-        for batch_data in loader:
-                # process data
-                batch_data = batch_data[0]
-                segSize = (batch_data['img_ori'].shape[0],
-                                   batch_data['img_ori'].shape[1])
-                img_resized_list = batch_data['img_data']
+		pbar = tqdm(total=len(loader))
+		for batch_data in loader:
+				# process data
+				batch_data = batch_data[0]
+				segSize = (batch_data['img_ori'].shape[0],
+								   batch_data['img_ori'].shape[1])
+				img_resized_list = batch_data['img_data']
 
-                with torch.no_grad():
-                        scores = torch.zeros(1, cfg.DATASET.num_class, segSize[0], segSize[1])
-                        scores = async_copy_to(scores, gpu)
+				with torch.no_grad():
+						scores = torch.zeros(1, cfg.DATASET.num_class, segSize[0], segSize[1])
+						scores = async_copy_to(scores, gpu)
 
-                        for img in img_resized_list:
-                                feed_dict = batch_data.copy()
-                                feed_dict['img_data'] = img
-                                del feed_dict['img_ori']
-                                del feed_dict['info']
-                                feed_dict = async_copy_to(feed_dict, gpu)
+						for img in img_resized_list:
+								feed_dict = batch_data.copy()
+								feed_dict['img_data'] = img
+								del feed_dict['img_ori']
+								del feed_dict['info']
+								feed_dict = async_copy_to(feed_dict, gpu)
 
-                                # forward pass
-                                pred_tmp = segmentation_module(feed_dict, segSize=segSize)
-                                scores = scores + pred_tmp / len(cfg.DATASET.imgSizes)
+								# forward pass
+								pred_tmp = segmentation_module(feed_dict, segSize=segSize)
+								scores = scores + pred_tmp / len(cfg.DATASET.imgSizes)
 
-                        _, pred = torch.max(scores, dim=1)
-                        pred = as_numpy(pred.squeeze(0).cpu())
+						_, pred = torch.max(scores, dim=1)
+						pred = as_numpy(pred.squeeze(0).cpu())
 
-                # visualization
-                return visualize_result(
-                        (batch_data['img_ori'], batch_data['info']),
-                        pred,
-                        cfg
-                )
+				# visualization
+				return visualize_result(
+						(batch_data['img_ori'], batch_data['info']),
+						pred,
+						cfg
+				)
 
-                pbar.update(1)
+				pbar.update(1)
 
 
-def main(cfg, gpu):
-        torch.cuda.set_device(gpu)
+def create_network(cfg, gpu):
+		torch.cuda.set_device(gpu)
 
-        # Network Builders
-        net_encoder = ModelBuilder.build_encoder(
-                arch=cfg.MODEL.arch_encoder,
-                fc_dim=cfg.MODEL.fc_dim,
-                weights=cfg.MODEL.weights_encoder)
-        net_decoder = ModelBuilder.build_decoder(
-                arch=cfg.MODEL.arch_decoder,
-                fc_dim=cfg.MODEL.fc_dim,
-                num_class=cfg.DATASET.num_class,
-                weights=cfg.MODEL.weights_decoder,
-                use_softmax=True)
+		# Network Builders
+		net_encoder = ModelBuilder.build_encoder(
+				arch=cfg.MODEL.arch_encoder,
+				fc_dim=cfg.MODEL.fc_dim,
+				weights=cfg.MODEL.weights_encoder)
+		net_decoder = ModelBuilder.build_decoder(
+				arch=cfg.MODEL.arch_decoder,
+				fc_dim=cfg.MODEL.fc_dim,
+				num_class=cfg.DATASET.num_class,
+				weights=cfg.MODEL.weights_decoder,
+				use_softmax=True)
 
-        crit = nn.NLLLoss(ignore_index=-1)
+		crit = nn.NLLLoss(ignore_index=-1)
 
-        segmentation_module = SegmentationModule(net_encoder, net_decoder, crit)
+		segmentation_module = SegmentationModule(net_encoder, net_decoder, crit)
 
-        # Dataset and Loader
-        dataset_test = TestDataset(
-                cfg.list_test,
-                cfg.DATASET)
-        loader_test = torch.utils.data.DataLoader(
-                dataset_test,
-                batch_size=cfg.TEST.batch_size,
-                shuffle=False,
-                collate_fn=user_scattered_collate,
-                num_workers=5,
-                drop_last=True)
+		segmentation_module.cuda()
 
-        segmentation_module.cuda()
+		# Main loop
+		return segmentation_module, gpu
 
-        # Main loop
-        return test(segmentation_module, loader_test, gpu)
-
-        print('Inference done!')
+		print('Inference done!')
 
 
 if __name__ == '__main__':
-        assert LooseVersion(torch.__version__) >= LooseVersion('0.4.0'), \
-                'PyTorch>=0.4.0 is required'
+		assert LooseVersion(torch.__version__) >= LooseVersion('0.4.0'), \
+				'PyTorch>=0.4.0 is required'
 
-        parser = argparse.ArgumentParser(
-                description="PyTorch Semantic Segmentation Testing"
-        )
-        parser.add_argument(
-                "--imgs",
-                required=True,
-                type=str,
-                help="an image path, or a directory name"
-        )
-        parser.add_argument(
-                "--cfg",
-                default="config/ade20k-resnet50dilated-ppm_deepsup.yaml",
-                metavar="FILE",
-                help="path to config file",
-                type=str,
-        )
-        parser.add_argument(
-                "--gpu",
-                default=0,
-                type=int,
-                help="gpu id for evaluation"
-        )
-        parser.add_argument(
-                "opts",
-                help="Modify config options using the command-line",
-                default=None,
-                nargs=argparse.REMAINDER,
-        )
-        args = parser.parse_args()
+		parser = argparse.ArgumentParser(
+				description="PyTorch Semantic Segmentation Testing"
+		)
+		parser.add_argument(
+				"--imgs",
+				required=True,
+				type=str,
+				help="an image path, or a directory name"
+		)
+		parser.add_argument(
+				"--cfg",
+				default="config/ade20k-resnet50dilated-ppm_deepsup.yaml",
+				metavar="FILE",
+				help="path to config file",
+				type=str,
+		)
+		parser.add_argument(
+				"--gpu",
+				default=0,
+				type=int,
+				help="gpu id for evaluation"
+		)
+		parser.add_argument(
+				"opts",
+				help="Modify config options using the command-line",
+				default=None,
+				nargs=argparse.REMAINDER,
+		)
+		args = parser.parse_args()
 
-        cfg.merge_from_file(args.cfg)
-        cfg.merge_from_list(args.opts)
-        # cfg.freeze()
+		cfg.merge_from_file(args.cfg)
+		cfg.merge_from_list(args.opts)
+		# cfg.freeze()
 
-        logger = setup_logger(distributed_rank=0)       # TODO
-        logger.info("Loaded configuration file {}".format(args.cfg))
-        logger.info("Running with config:\n{}".format(cfg))
+		logger = setup_logger(distributed_rank=0)		# TODO
+		logger.info("Loaded configuration file {}".format(args.cfg))
+		logger.info("Running with config:\n{}".format(cfg))
 
-        cfg.MODEL.arch_encoder = cfg.MODEL.arch_encoder.lower()
-        cfg.MODEL.arch_decoder = cfg.MODEL.arch_decoder.lower()
+		cfg.MODEL.arch_encoder = cfg.MODEL.arch_encoder.lower()
+		cfg.MODEL.arch_decoder = cfg.MODEL.arch_decoder.lower()
 
-        # absolute paths of model weights
-        cfg.MODEL.weights_encoder = os.path.join(
-                cfg.DIR, 'encoder_' + cfg.TEST.checkpoint)
-        cfg.MODEL.weights_decoder = os.path.join(
-                cfg.DIR, 'decoder_' + cfg.TEST.checkpoint)
-        # generate testing image list
-        if os.path.isdir(args.imgs):
-                imgs = find_recursive(args.imgs)
-        else:
-                imgs = [args.imgs]
-        assert len(imgs), "imgs should be a path to image (.jpg) or directory."
-        cfg.list_test = [{'fpath_img': x} for x in imgs]
+		# absolute paths of model weights
+		cfg.MODEL.weights_encoder = os.path.join(
+				cfg.DIR, 'encoder_' + cfg.TEST.checkpoint)
+		cfg.MODEL.weights_decoder = os.path.join(
+				cfg.DIR, 'decoder_' + cfg.TEST.checkpoint)
+		# generate testing image list
+		if os.path.isdir(args.imgs):
+				imgs = find_recursive(args.imgs)
+		else:
+				imgs = [args.imgs]
+		assert len(imgs), "imgs should be a path to image (.jpg) or directory."
+		cfg.list_test = [{'fpath_img': x} for x in imgs]
 
-        if not os.path.isdir(cfg.TEST.result):
-                os.makedirs(cfg.TEST.result)
+		if not os.path.isdir(cfg.TEST.result):
+				os.makedirs(cfg.TEST.result)
 
-        main(cfg, args.gpu)
+		main(cfg, args.gpu)
 
-def detect(imgs):
-        cfg.merge_from_file("config/ade20k-resnet50dilated-ppm_deepsup.yaml")
-        cfg.merge_from_list([])
-        cfg.MODEL.arch_encoder = cfg.MODEL.arch_encoder.lower()
-        cfg.MODEL.arch_decoder = cfg.MODEL.arch_decoder.lower()
-        cfg.MODEL.weights_encoder = os.path.join(
-                cfg.DIR, 'encoder_' + cfg.TEST.checkpoint)
-        cfg.MODEL.weights_decoder = os.path.join(
-                cfg.DIR, 'decoder_' + cfg.TEST.checkpoint)
-        assert os.path.exists(cfg.MODEL.weights_encoder) and \
-                os.path.exists(cfg.MODEL.weights_decoder), "checkpoint does not exitst!"
-        if os.path.isdir(imgs):
-                imgs = find_recursive(imgs)
-        else:
-                imgs = [imgs]
-        assert len(imgs), "imgs should be a path to image (.jpg) or directory."
-        cfg.list_test = [{'fpath_img': x} for x in imgs]
-        if not os.path.isdir(cfg.TEST.result):
-                os.makedirs(cfg.TEST.result)
-        return main(cfg, 0)
+def create_dataset():
+	dataset_test = TestDataset(
+				cfg.list_test,
+				cfg.DATASET)
+	loader_test = torch.utils.data.DataLoader(
+				dataset_test,
+				batch_size=cfg.TEST.batch_size,
+				shuffle=False,
+				collate_fn=user_scattered_collate,
+				num_workers=5,
+				drop_last=True)
+	return loader_test
+
+#Initializing our network, gloabally
+
+cfg.merge_from_file("config/ade20k-resnet50dilated-ppm_deepsup.yaml")
+cfg.merge_from_list([])
+cfg.MODEL.arch_encoder = cfg.MODEL.arch_encoder.lower()
+cfg.MODEL.arch_decoder = cfg.MODEL.arch_decoder.lower()
+cfg.MODEL.weights_encoder = os.path.join(
+		cfg.DIR, 'encoder_' + cfg.TEST.checkpoint)
+cfg.MODEL.weights_decoder = os.path.join(
+		cfg.DIR, 'decoder_' + cfg.TEST.checkpoint)
+assert os.path.exists(cfg.MODEL.weights_encoder) and \
+		os.path.exists(cfg.MODEL.weights_decoder), "checkpoint does not exitst!"
+segmentation_module, gpu = create_network(cfg, 0)
+
+def detect(img):
+	cfg.list_test = [{'fpath_img': img}]
+	loader_test = create_dataset()
+	return test(segmentation_module, loader_test, gpu)
