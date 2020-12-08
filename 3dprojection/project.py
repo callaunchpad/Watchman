3ad048ec6616c3b3b53f3dfc21b9b6604b083cd5
Sumer Kohli <sumer.kohli@berkeley.edu>
2020-12-07 16:09:34 -0800

    Implement ability to record and play back camera trajectories

--- 3dprojection/project.py
+++ 3dprojection/project.py
@@ -1,5 +1,6 @@
 # Required Imports
 import sys
+
 sys.path.append("../")
 import os
 import numpy as np
@@ -10,7 +11,9 @@ import importlib.util
 from yolo.detect import get_bounding_boxes
 import time
 import json
+
 CALIB_DIR = "../data/salsa/calib"
+
 
 def dynamic_load(source_path):
     spec = importlib.util.spec_from_file_location(
@@ -19,6 +22,7 @@ def dynamic_load(source_path):
     module_object = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module_object)
     return module_object
+
 
 def get_oriented_boxes(depth_im, intrinsics, coords):
     bounding_boxes = []
@@ -32,11 +36,17 @@ def get_oriented_boxes(depth_im, intrinsics, coords):
         height = top_left[1] - bottom_right[1]
         width = top_left[0] - bottom_right[0]
         z1 = depth_im[int(center_y)][int(center_x)]
-        pt_x1 = (center_x - ints.intrinsic_matrix[0, 2]) * z1 / ints.intrinsic_matrix[0, 0]
-        pt_y1 = (center_y - ints.intrinsic_matrix[1, 2]) * z1 / ints.intrinsic_matrix[1, 1]
-        mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.05, origin=[-pt_x1, pt_y1, z1])
+        pt_x1 = (
+            (center_x - ints.intrinsic_matrix[0, 2]) * z1 / ints.intrinsic_matrix[0, 0]
+        )
+        pt_y1 = (
+            (center_y - ints.intrinsic_matrix[1, 2]) * z1 / ints.intrinsic_matrix[1, 1]
+        )
+        mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+            size=0.05, origin=[pt_x1, -pt_y1, -z1]
+        )
         bounding_boxes.append(mesh_frame)
-        
+
     return bounding_boxes
 
 
@@ -76,25 +86,50 @@ if __name__ == "__main__":
         depth_im = np.load(eval('f"' + depth_map + '"'))
         depth_im_temp = depth_im
         depth_im = o3d.geometry.Image(depth_im)
-        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color_im, depth_im, depth_scale = 1, convert_rgb_to_intensity = False)
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            color_im, depth_im, depth_scale=1, convert_rgb_to_intensity=False
+        )
 
         curr_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, ints)
         curr_pcd = curr_pcd.voxel_down_sample(voxel_size=0.001)
         curr_pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-        vis.add_geometry(curr_pcd)
+
+        geometries = []
+        geometries.append(curr_pcd)
+        # vis.add_geometry(curr_pcd)
 
         # os.chdir("/Users/Mokshith/Documents/launchpad/Watchman/3dprojection")
-        frame_locations = sorted(os.listdir("./cam1_frames/"), key = lambda x: int(x.replace("frame", "").replace(".jpg", "")))
-        
+        frame_locations = sorted(
+            os.listdir("./cam1_frames/"),
+            key=lambda x: int(x.replace("frame", "").replace(".jpg", "")),
+        )
+
         coords = get_bounding_boxes(img)
-        bounding_boxes = get_oriented_boxes(depth_im_temp, ints, coords) + get_oriented_boxes(depth_im_temp, ints, coords)
+        bounding_boxes = get_oriented_boxes(
+            depth_im_temp, ints, coords
+        ) + get_oriented_boxes(depth_im_temp, ints, coords)
         for box in bounding_boxes:
-            vis.add_geometry(box)
-        
-        curr_loc = 0
+            geometries.append(box)
+            # vis.add_geometry(box)
+
+        curr_loc = -1
         changer = 1
 
         camera_params = []
+
+        for geom in geometries:
+            vis.add_geometry(geom)
+        cur_trajectory = o3d.io.read_pinhole_camera_trajectory(
+            os.path.abspath("./traj2.json")
+        )
+        print(
+            "Trajectory:",
+            cur_trajectory,
+        )
+        # o3d.visualization.draw_geometries_with_custom_animation(
+        #     geometries,
+        #     optional_view_trajectory_json_file=os.path.abspath("./traj2.json"),
+        # )
 
         def move_forward(vis):
             global curr_loc
@@ -103,52 +138,82 @@ if __name__ == "__main__":
             global changer
 
             curr_loc += 1
-            if curr_loc % 100: return
+            if curr_loc % 5:
+                return
 
             ctr = vis.get_view_control()
 
-            cur_params = ctr.convert_to_pinhole_camera_parameters()
-            camera_params.append(cur_params)
-            print(time.time())
-
-            if curr_loc // 100 > 20:
-                vis.register_animation_callback(None)
-                print("Done sampling")
-                with open("camera_trajectory.out.json", "w") as fp:
-                    json.dump(camera_params, fp)
-            
-            return
-
-            if curr_loc % 300 == 0:
-                changer *= -1
-            
-            if curr_loc == 0:
-                ctr.rotate(100, 0)
+            if True:
+                ctr.convert_from_pinhole_camera_parameters(
+                    cur_trajectory.parameters[
+                        (curr_loc // 5) % len(cur_trajectory.parameters)
+                    ]
+                )
             else:
-                ctr.rotate(changer * 1.0, 0.0)
+                cur_params = ctr.convert_to_pinhole_camera_parameters()
+                camera_params.append(cur_params)
+                print(time.time())
 
-            
+                if curr_loc // 100 > 10:
+                    vis.register_animation_callback(None)
+                    print("Done sampling")
+                    out_params = [
+                        (
+                            {
+                                "class_name": "PinholeCameraParameters",
+                                "extrinsic": params.extrinsic.flatten().tolist(),
+                                "intrinsic": {
+                                    "height": params.intrinsic.height,
+                                    "width": params.intrinsic.width,
+                                    "intrinsic_matrix": params.intrinsic.intrinsic_matrix.flatten().tolist(),
+                                },
+                            }
+                        )
+                        for params in camera_params
+                    ]
+                    out_params = {
+                        "class_name": "PinholeCameraTrajectory",
+                        "parameters": out_params,
+                        "version_major": 1,
+                        "version_minor": 0,
+                    }
+                    traj = o3d.camera.PinholeCameraTrajectory()
+                    traj.parameters = camera_params
+                    with open("camera_trajectory.out.json", "w") as fp:
+                        json.dump(out_params, fp)
+                    o3d.io.write_pinhole_camera_trajectory("traj2.json", traj)
+
+            # if curr_loc % 300 == 0:
+            #     changer *= -1
+
+            # if curr_loc == 0:
+            #     ctr.rotate(100, 0)
+            # else:
+            #     ctr.rotate(changer * 1.0, 0.0)
 
             if curr_loc % 100 == 0:
                 # coords = get_bounding_boxes(os.path.join("cam1_frames/", frame_locations[curr_loc]))
-                coords = np.load(os.path.join("cam1_preds/", frame_locations[curr_loc//100]).replace("jpg", "npy"))
+                coords = np.load(
+                    os.path.join(
+                        "cam1_preds/", frame_locations[curr_loc // 100]
+                    ).replace("jpg", "npy")
+                )
                 # np.save(os.path.join("cam1_preds/", frame_locations[curr_loc]).replace("jpg", "npy"), coords)
                 bounding_boxes_temp = get_oriented_boxes(depth_im_temp, ints, coords)
 
                 for i in range(len(bounding_boxes_temp)):
                     bounding_boxes[i].paint_uniform_color(np.array([0, 1, 0]))
-                    bounding_boxes[i].translate(bounding_boxes_temp[i].get_center(), relative = False)
-                
+                    bounding_boxes[i].translate(
+                        bounding_boxes_temp[i].get_center(), relative=False
+                    )
+
                 for i in range(len(bounding_boxes_temp), len(bounding_boxes)):
                     bounding_boxes[i].paint_uniform_color(np.array([0, 0, 0]))
-                    bounding_boxes[i].translate(np.array([1, 1, 1]), relative = False)
-                
+                    bounding_boxes[i].translate(np.array([1, 1, 1]), relative=False)
+
                 for geom in bounding_boxes:
                     vis.update_geometry(geom)
-                
-            
-            curr_loc += 1
-        
+
         vis.register_animation_callback(move_forward)
         vis.run()
         vis.destroy_window()
