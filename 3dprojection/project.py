75d4e432246113701606d7b38e77b2d2b5ac6002
Mokshith Voodarla <mvoodarla@gmail.com>
2020-12-07 18:22:35 -0800

    add sumer's trajectory stuff
    Co-authored-by: Mokshith Voodarla <mvoodarla@gmail.com>
    Co-authored-by: name <sumer.kohli@berkeley.edu>

--- 3dprojection/project.py
+++ 3dprojection/project.py
@@ -9,9 +9,10 @@ import argparse
 import importlib.util
 from yolo.detect import get_bounding_boxes
 import time
+import importlib.util
+import json
 
 CALIB_DIR = "../data/salsa/calib"
-DATASET = 'salsa'
 
 def dynamic_load(source_path):
     spec = importlib.util.spec_from_file_location(
@@ -33,15 +34,24 @@ def get_oriented_boxes(depth_im, intrinsics, coords):
         height = top_left[1] - bottom_right[1]
         width = top_left[0] - bottom_right[0]
         z1 = depth_im[int(center_y)][int(center_x)]
-        pt_x1 = (center_x - ints.intrinsic_matrix[0, 2]) * z1 / ints.intrinsic_matrix[0, 0]
-        pt_y1 = (center_y - ints.intrinsic_matrix[1, 2]) * z1 / ints.intrinsic_matrix[1, 1]
-        mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.05, origin=[-pt_x1, pt_y1, z1])
+        pt_x1 = (
+            (center_x - ints.intrinsic_matrix[0, 2]) * z1 / ints.intrinsic_matrix[0, 0]
+        )
+        pt_y1 = (
+            (center_y - ints.intrinsic_matrix[1, 2]) * z1 / ints.intrinsic_matrix[1, 1]
+        )
+        mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+            size=0.05, origin=[-pt_x1, pt_y1, z1]
+        )
         bounding_boxes.append(mesh_frame)
-        
+
     return bounding_boxes
 
 
 if __name__ == "__main__":
+    DATASET = sys.argv[1]
+    record = sys.argv[2]
+
     if DATASET == 'lab':
         sources = [
             (
@@ -90,10 +100,11 @@ if __name__ == "__main__":
         depth_im = o3d.geometry.Image(depth_im)
         rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color_im, depth_im, depth_scale = 1, convert_rgb_to_intensity = False)
 
+        geometries = []
         curr_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, ints)
         curr_pcd = curr_pcd.voxel_down_sample(voxel_size=0.001)
         curr_pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-        vis.add_geometry(curr_pcd)
+        geometries.append(curr_pcd)
 
         os.chdir("/Users/Mokshith/Documents/launchpad/Watchman/3dprojection")
         
@@ -108,10 +119,23 @@ if __name__ == "__main__":
         coords = get_bounding_boxes('cam1_frames/frame0.jpg' if DATASET == 'salsa' else 'lab_frames/003200.jpg')
         bounding_boxes = get_oriented_boxes(depth_im_temp, ints, coords) + get_oriented_boxes(depth_im_temp, ints, coords) + get_oriented_boxes(depth_im_temp, ints, coords)
         for box in bounding_boxes:
-            vis.add_geometry(box)
+            geometries.append(box)
         
-        curr_loc = 0
+        curr_loc = -1
         changer = 1
+
+        camera_params = []
+
+        for geom in geometries:
+            vis.add_geometry(geom)
+        cur_trajectory = o3d.io.read_pinhole_camera_trajectory(
+            os.path.abspath("./{}_trajectory.json".format(DATASET))
+        )
+
+        print(
+            "Trajectory:",
+            cur_trajectory,
+        )
 
         def move_forward(vis):
             global curr_loc
@@ -119,18 +143,54 @@ if __name__ == "__main__":
             global bounding_boxes
             global changer
 
+            curr_loc += 1
+            if curr_loc % 5:
+                return
+
             ctr = vis.get_view_control()
 
-            if curr_loc % 300 == 0:
-                changer *= -1
-            
-            if curr_loc == 0:
-                ctr.rotate(100, 0)
+            if record == "play":
+                ctr.convert_from_pinhole_camera_parameters(
+                    cur_trajectory.parameters[
+                        (curr_loc // 5) % len(cur_trajectory.parameters)
+                    ]
+                )
             else:
-                ctr.rotate(changer * 1.0, 0.0)
+                cur_params = ctr.convert_to_pinhole_camera_parameters()
+                camera_params.append(cur_params)
+                print(time.time())
 
-            if curr_loc % (10 if DATASET == 'salsa' else 10) == 0:
-                coords = np.load(os.path.join("{}_preds/".format(frame_folder), frame_locations[curr_loc//(10 if DATASET == 'salsa' else 10)]).replace("jpg", "npy"))
+                if curr_loc // 100 > 10:
+                    vis.register_animation_callback(None)
+                    print("Done sampling")
+                    out_params = [
+                        (
+                            {
+                                "class_name": "PinholeCameraParameters",
+                                "extrinsic": params.extrinsic.flatten().tolist(),
+                                "intrinsic": {
+                                    "height": params.intrinsic.height,
+                                    "width": params.intrinsic.width,
+                                    "intrinsic_matrix": params.intrinsic.intrinsic_matrix.flatten().tolist(),
+                                },
+                            }
+                        )
+                        for params in camera_params
+                    ]
+                    out_params = {
+                        "class_name": "PinholeCameraTrajectory",
+                        "parameters": out_params,
+                        "version_major": 1,
+                        "version_minor": 0,
+                    }
+                    traj = o3d.camera.PinholeCameraTrajectory()
+                    traj.parameters = camera_params
+                    with open("camera_trajectory.out.json", "w") as fp:
+                        json.dump(out_params, fp)
+                    o3d.io.write_pinhole_camera_trajectory("./{}_trajectory.json".format(DATASET), traj)
+
+            if curr_loc % (10 if DATASET == 'salsa' else 20) == 0:
+                coords = np.load(os.path.join("{}_preds/".format(frame_folder), frame_locations[curr_loc//(10 if DATASET == 'salsa' else 20)]).replace("jpg", "npy"))
                 bounding_boxes_temp = get_oriented_boxes(depth_im_temp, ints, coords)
 
                 for i in range(len(bounding_boxes_temp)):
