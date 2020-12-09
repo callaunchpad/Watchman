c5c47a8a8dd9f4bbd030661c3a57744a18f106bb
Sumer Kohli <sumer.kohli@berkeley.edu>
2020-12-08 17:14:20 -0800

    Fix bounding box origin, frame location array batching

--- 3dprojection/project.py
+++ 3dprojection/project.py
@@ -1,5 +1,6 @@
 # Required Imports
 import sys
+
 sys.path.append("../")
 import os
 import numpy as np
@@ -14,6 +15,7 @@ import json
 
 CALIB_DIR = "../data/salsa/calib"
 
+
 def dynamic_load(source_path):
     spec = importlib.util.spec_from_file_location(
         f"dynamic_source_{source_path}", source_path
@@ -21,6 +23,7 @@ def dynamic_load(source_path):
     module_object = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module_object)
     return module_object
+
 
 def get_oriented_boxes(depth_im, intrinsics, coords):
     bounding_boxes = []
@@ -41,7 +44,7 @@ def get_oriented_boxes(depth_im, intrinsics, coords):
             (center_y - ints.intrinsic_matrix[1, 2]) * z1 / ints.intrinsic_matrix[1, 1]
         )
         mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
-            size=0.05, origin=[-pt_x1, pt_y1, z1]
+            size=0.05, origin=[pt_x1, -pt_y1, -z1]
         )
         bounding_boxes.append(mesh_frame)
 
@@ -52,7 +55,7 @@ if __name__ == "__main__":
     DATASET = sys.argv[1]
     record = sys.argv[2]
 
-    if DATASET == 'lab':
+    if DATASET == "lab":
         sources = [
             (
                 os.path.abspath(os.path.join(CALIB_DIR, "cam1_lab.py")),
@@ -61,7 +64,7 @@ if __name__ == "__main__":
                 os.path.abspath("../data/salsa/test_depth/lab_depth.npy"),
             )
         ]
-    elif DATASET == 'salsa':
+    elif DATASET == "salsa":
         sources = [
             (
                 os.path.abspath(os.path.join(CALIB_DIR, "cam1.py")),
@@ -70,7 +73,6 @@ if __name__ == "__main__":
                 os.path.abspath("../data/salsa/test_depth/0_1_disp.npy"),
             )
         ]
-
 
     vis = o3d.visualization.Visualizer()
     vis.create_window()
@@ -98,7 +100,9 @@ if __name__ == "__main__":
         depth_im = np.load(eval('f"' + depth_map + '"'))
         depth_im_temp = depth_im
         depth_im = o3d.geometry.Image(depth_im)
-        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color_im, depth_im, depth_scale = 1, convert_rgb_to_intensity = False)
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            color_im, depth_im, depth_scale=1, convert_rgb_to_intensity=False
+        )
 
         geometries = []
         curr_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, ints)
@@ -106,21 +110,30 @@ if __name__ == "__main__":
         curr_pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
         geometries.append(curr_pcd)
 
-        os.chdir("/Users/Mokshith/Documents/launchpad/Watchman/3dprojection")
-        
-        frame_folder = 'cam1' if DATASET == 'salsa' else 'lab'
+        # os.chdir("/Users/Mokshith/Documents/launchpad/Watchman/3dprojection")
 
-        if DATASET == 'salsa':
-            frame_locations = sorted(os.listdir("./cam1_frames/"), key = lambda x: int(x.replace("frame", "").replace(".jpg", "")))
-        elif DATASET == 'lab':
+        frame_folder = "cam1" if DATASET == "salsa" else "lab"
+
+        if DATASET == "salsa":
+            frame_locations = sorted(
+                os.listdir("./cam1_frames/"),
+                key=lambda x: int(x.replace("frame", "").replace(".jpg", "")),
+            )
+        elif DATASET == "lab":
             frame_locations = sorted(os.listdir("./{}_frames/".format(frame_folder)))
-            frame_locations = frame_locations[0::10]
-        
-        coords = get_bounding_boxes('cam1_frames/frame0.jpg' if DATASET == 'salsa' else 'lab_frames/003200.jpg')
-        bounding_boxes = get_oriented_boxes(depth_im_temp, ints, coords) + get_oriented_boxes(depth_im_temp, ints, coords) + get_oriented_boxes(depth_im_temp, ints, coords)
+            # frame_locations = frame_locations[0::10]
+
+        coords = get_bounding_boxes(
+            "cam1_frames/frame0.jpg" if DATASET == "salsa" else "lab_frames/003200.jpg"
+        )
+        bounding_boxes = (
+            get_oriented_boxes(depth_im_temp, ints, coords)
+            + get_oriented_boxes(depth_im_temp, ints, coords)
+            + get_oriented_boxes(depth_im_temp, ints, coords)
+        )
         for box in bounding_boxes:
             geometries.append(box)
-        
+
         curr_loc = -1
         changer = 1
 
@@ -187,26 +200,34 @@ if __name__ == "__main__":
                     traj.parameters = camera_params
                     with open("camera_trajectory.out.json", "w") as fp:
                         json.dump(out_params, fp)
-                    o3d.io.write_pinhole_camera_trajectory("./{}_trajectory.json".format(DATASET), traj)
+                    o3d.io.write_pinhole_camera_trajectory(
+                        "./{}_trajectory.json".format(DATASET), traj
+                    )
 
-            if curr_loc % (10 if DATASET == 'salsa' else 20) == 0:
-                coords = np.load(os.path.join("{}_preds/".format(frame_folder), frame_locations[curr_loc//(10 if DATASET == 'salsa' else 20)]).replace("jpg", "npy"))
+            if curr_loc % (10 if DATASET == "salsa" else 20) == 0:
+                coords = np.load(
+                    os.path.join(
+                        "{}_preds/".format(frame_folder),
+                        frame_locations[curr_loc // (10 if DATASET == "salsa" else 20)],
+                    ).replace("jpg", "npy")
+                )
                 bounding_boxes_temp = get_oriented_boxes(depth_im_temp, ints, coords)
 
                 for i in range(len(bounding_boxes_temp)):
                     bounding_boxes[i].paint_uniform_color(np.array([0, 1, 0]))
-                    bounding_boxes[i].translate(bounding_boxes_temp[i].get_center(), relative = False)
-                
+                    bounding_boxes[i].translate(
+                        bounding_boxes_temp[i].get_center(), relative=False
+                    )
+
                 for i in range(len(bounding_boxes_temp), len(bounding_boxes)):
                     bounding_boxes[i].paint_uniform_color(np.array([0, 0, 0]))
-                    bounding_boxes[i].translate(np.array([1, 1, 1]), relative = False)
-                
+                    bounding_boxes[i].translate(np.array([1, 1, 1]), relative=False)
+
                 for geom in bounding_boxes:
                     vis.update_geometry(geom)
-                
-            
+
             curr_loc += 1
-        
+
         vis.register_animation_callback(move_forward)
         vis.run()
         vis.destroy_window()
